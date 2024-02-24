3df8e5bcca2e1954b2c56655d30469b3c3c0c80f
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-24 09:22:57 +0000

    setup express app

---
+++ server.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const express = require('express');
+const { nextTick } = require('process');
+
+//  express app
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(`${req.method}, ${req.url}, ${req.path}`);
+    next()
+})
+
+// routes
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome to the app'});
+});
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+  console.log(`listening on port ${process.env.PORT}`);
+});
+
+process.env
