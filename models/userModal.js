d0f34211edb7687f2e4d295e2574bc4b10c6ee68
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-02-24 19:28:54 +0000

    added model and gitignore

---
+++ models/userModal.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+  },
+//   adds a time created field to the schema
+// also adds time updated field to the schema - possibly, need to check
+  { timestamps: true }
+);
+
+// automatically creates a collection. Pluralises the name of the model
+module.exports = mongoose.model('User', userSchema);
+
+
--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,42 @@
 const express = require('express');
+const User = require('../models/userModal');
 
 const router = express.Router();
 
 // GET all users
 router.get('/', (req, res) => {
-    res.json({message: 'GET all users'})
-})
+  res.json({ message: 'GET all users' });
+});
 
 // GET a single user
 router.get('/:id', (req, res) => {
-    res.json({message: 'GET a single user'})
-})
+  res.json({ message: 'GET a single user' });
+});
 
 // POST a new user
-router.post('/', (req, res) => {
-    res.json({message: 'POST a new user'})
-})
+router.post('/', async (req, res) => {
+  const { name, email, description, location } = req.body;
+  try {
+    const user = await User.create({
+      name,
+      email,
+      description,
+      location,
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 // DELETE a new user
 router.delete('/:id', (req, res) => {
-    res.json({message: 'DELETE a new user'})
-})
-
+  res.json({ message: 'DELETE a new user' });
+});
 
 // UPDATE a new user
 router.patch('/:id', (req, res) => {
-    res.json({message: 'UPDATE a new user'})
-})
-
-
+  res.json({ message: 'UPDATE a new user' });
+});
 
 module.exports = router;
