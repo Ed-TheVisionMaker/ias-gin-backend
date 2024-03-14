5d1a53b3125f51e2c15d0d208eb9b7fed872ce91
Ed Houlcroft <ed.lc.bwi@gmail.com>
2024-03-14 20:22:52 +0000

    feat(waitlist): waitList Schema and registeration static

---
+++ models/waitListModal.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+const validator = require('validator');
+
+const Schema = mongoose.Schema;
+
+const waitListSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+waitListSchema.static.register = async function (email) {
+  if (!validator.isEmail(email)) {
+    throw createCustomError('Email is invalid', 'ValidationError');
+  }
+
+  const exists = await this.findOne({ email });
+
+  if (exists) {
+    throw createCustomError('Email already registered', 'ValidationError');
+  }
+  const waitList = await this.create({
+    email,
+  });
+  return waitList;
+};
